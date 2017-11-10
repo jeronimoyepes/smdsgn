858bb02237cb9edffcf87957c27275eef852fe18
unknown <jeronimo_y_g@hotmail.com>
2017-11-09 22:05:13 -0500

    home terminado

--- dist/js/styling.js
+++ dist/js/styling.js
@@ -2,11 +2,19 @@ $(document).ready(function($) {
     
     // registrar el cambio de tamaño de la pantalla
     var winWidth = $(window).width();
-    $(window).resize(function resizeActivated () {
-        console.log("hola");
-        if ($(this).width() != winWidth && $(this).width() > 991) {
+    $(window).resize(function () {
+        // cuando el tamaño de la pantalla cambie y sea mayor a 991px el height sea igual al del banner
+        if ($(this).width() != winWidth && $(this).width() > 992) {
             $(".banner .container .video iframe").height($(".cards").height());
         }
+        else if ($(this).width() < 992) {
+            $(".banner .container .video iframe").height(300);
+        }
     });
-    $(".banner .container .video iframe").height($(".cards").height());
+    // las mismas reglas de arriba pero por fuera para que se ejecuten apenas cargue la página
+    if (winWidth > 992) {
+        $(".banner .container .video iframe").height($(".cards").height());
+    }else if (winWidth < 992) {
+        $(".banner .container .video iframe").height(300);
+    }  
 });
