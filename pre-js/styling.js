bb8a560054154f81eb828255a495fbba5fb158a3
unknown <jeronimo_y_g@hotmail.com>
2017-11-17 01:00:34 -0500

    Pantalla programas

--- pre-js/styling.js
+++ pre-js/styling.js
@@ -10,11 +10,32 @@ $(document).ready(function($) {
         else if ($(this).width() < 992) {
             $(".banner .container .video iframe").height(300);
         }
+
+        // que el alto de las imagenes de los programas en la pestaña programas sea igual al alto de las descripciones
+        if (winWidth > 900) {
+            $(".program-cont .program-sect .image").each(function (i) {
+                $(this).height($(this).prev().height())
+            });
+        }
+        else if ($(this).width() < 992) {
+            $(".program-cont .program-sect .image").height(350);
+        }
     });
+    
     // las mismas reglas de arriba pero por fuera para que se ejecuten apenas cargue la página
     if (winWidth > 992) {
         $(".banner .container .video iframe").height($(".cards").height());
-    }else if (winWidth < 992) {
+    }else if ($(this).width() < 992) {
         $(".banner .container .video iframe").height(300);
-    }  
+    } 
+    
+    // imagenes de programas en la pestaña programas
+    if (winWidth > 900) {
+        $(".program-cont .program-sect .image").each(function (i) {
+            $(this).height($(this).prev().height())
+        });
+    }
+    else if ($(this).width() < 992) {
+        $(".program-cont .program-sect .image").height(350);
+    }
 });
